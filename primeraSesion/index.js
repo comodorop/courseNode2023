9f86df894354361406a85d54d46a463e67179861
comodorop <comodoro_21@hotmail.com>
2023-04-29 10:38:23 -0600

    add post user

--- primeraSesion/index.js
+++ primeraSesion/index.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 const express = require("express")
 const bodyParser = require('body-parser')
 const courses = require("./controller/courses.controller")
+const users = require("./controller/users.controller")
 const app = express()
 const { validate } = require("./midlewares/middleware")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use("/v1/courses",  courses)
+app.use("/v1/users", users )
 // http: GET,  DELETE, POST, PUT, PATCH
 // HTTP STATUS: 200, 201, 404, 401, 500
 app.listen(process.env.PORT, () => {
---
+++ primeraSesion/repository/users.repository.js
@@ -0,0 +1,12 @@
+const { getConnection } = require("../connection/connection")
+
+
+
+async function save(body){
+    let knex = getConnection()
+    return await knex.insert(body).from('user').returning('uuid')
+}
+
+module.exports={
+    save
+}
